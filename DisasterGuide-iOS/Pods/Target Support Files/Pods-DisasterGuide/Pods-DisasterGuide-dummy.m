b2fc21041b43fa620021e53731c874a38d10b2e0
gypang <gypang24@gmail.com>
2018-09-15 15:47:33 -0400

    Put everything into an iOS folder

---
+++ DisasterGuide-iOS/Pods/Target Support Files/Pods-DisasterGuide/Pods-DisasterGuide-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DisasterGuide : NSObject
+@end
+@implementation PodsDummy_Pods_DisasterGuide
+@end
---
+++ DisasterGuide-iOS/Pods/Target Support Files/Pods-DisasterGuideUITests/Pods-DisasterGuideUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DisasterGuideUITests : NSObject
+@end
+@implementation PodsDummy_Pods_DisasterGuideUITests
+@end
