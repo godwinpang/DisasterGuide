0cb46d32abd312f4fd8110b375b80880b1525ddd
gypang <gypang24@gmail.com>
2018-09-16 05:19:30 -0400

    Add persistence of userid

--- DisasterGuide-iOS/DisasterGuide/SignupViewController.swift
+++ DisasterGuide-iOS/DisasterGuide/SignupViewController.swift
@@ -8,11 +8,14 @@
 
 import UIKit
 import Alamofire
+import SwiftyJSON
 
 class SignupViewController: UIViewController {
 
     @IBOutlet weak var nameField: UITextField!
     @IBOutlet weak var signUpButton: UIButton!
+    
+    let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,8 +32,53 @@ class SignupViewController: UIViewController {
     }
     
     @IBAction func onSignUp(_ sender: Any) {
-        //Alamofire.request()
+        
+        let user_name = nameField.text ?? "Thomas"
+        
+        
+        Alamofire.request(Constants().serverURL+"/getallusers").responseJSON { response in
+            /*switch response.result {
+             //get the JSON
+             case .success(let value):
+             let jsonString = JSON(value)
+             print(jsonString)
+             //returnArr = self.parseJSON(json: jsonString)
+             case .failure(let error):
+             print(error)
+             returnArr = ["failure"]
+             }
+             */
+            let jsonString = response.result.value
+            print(jsonString)
+            
+        }
+        
+        let parameters: Parameters = [
+            "first_name": user_name,
+            "last_name": "Spencer",
+            "birthday": [
+                "month": 1,
+                "day": 20,
+                "year": 1998
+            ],
+            "role": "user"
+        ]
+        /*
+        Alamofire.request(Constants().serverURL + "/adduser", method: .post, parameters: parameters, encoding: JSONEncoding.default).responseJSON { response in
+            let jsonString = response.result as? String
+            print(jsonString)
+            let jsonObj = JSON(parseJSON: jsonString!)
+            print(jsonObj["user_id"])
+            let user_id = jsonObj["user_id"]
+        }
+        */
+        
+        
         self.performSegue(withIdentifier: "signUpSegue", sender: nil)
+        
+        //defaults.set(user_id, forKey: "user_id")
+        defaults.set("user", forKey: "role")
+        
         /*
         let username = nameField.text ?? "breaking"
         // TODO will trash if usernameString is empty
