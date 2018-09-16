917b10f92bf787c79e7c6c7aac99df0cbe3d09b8
D1o0g9s <boodalina@gmail.com>
2018-09-15 22:59:31 -0400

    Started Speech to Text tester

---
+++ TestSpeechToText/TestSpeechToText/ViewController.swift
@@ -0,0 +1,81 @@
+//
+//  ViewController.swift
+//  TestSpeechToText
+//
+//  Created by Geeling Chau on 9/15/18.
+//  Copyright © 2018 Geeling Chau. All rights reserved.
+//
+
+import UIKit
+import Speech
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var textLabel: UILabel!
+    
+    var isListening = false
+    let audioEngine = AVAudioEngine()
+    let speechRecognizer: SFSpeechRecognizer? = SFSpeechRecognizer()
+    
+    let request = SFSpeechAudioBufferRecognitionRequest()
+    var recognitionTask: SFSpeechRecognitionTask?
+    
+
+
+    
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func toggleS2T(_ sender: Any) {
+        if(isListening) {
+            isListening = false
+        } else {
+            isListening = true
+            self.recordAndRecognizeSpeech()
+            
+        }
+        textLabel.text = "mwahahahahaha"
+    }
+    
+    func recordAndRecognizeSpeech() {
+        let node = audioEngine.inputNode
+        let recordingFormat = node.outputFormat(forBus:0)
+        node.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { buffer, _ in self.request.append(buffer)
+        }
+        
+        audioEngine.prepare()
+        do {
+            try audioEngine.start()
+        } catch {
+            return print(error)
+        }
+        
+        guard let myRecognizer = SFSpeechRecognizer() else { return }
+        
+        if !myRecognizer.isAvailable {
+            return
+        }
+        
+        recognitionTask = speechRecognizer?.recognitionTask(with: request, resultHandler: { result, error in
+            if let result = result {
+                let bestString = result.bestTranscription.formattedString
+                self.textLabel.text = bestString
+                
+            } else if let error = error {
+                print(error)
+                
+            }})
+        
+    }
+    
+}
+
